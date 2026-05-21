4a9b2725a18fb381620bd925a2a1d505359361b8
i32mumaj <i32mumaj@uco.es>
2026-05-21 14:50:51 +0200

    Eliminado código Java Spring Boot y componentes React anteriores

--- backend/src/main/java/com/c2c/market/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.c2c.market.repository;
-
-import com.c2c.market.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
